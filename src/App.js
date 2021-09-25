d750d7c0e804a50923d7950ff1b1349e6dd80683
Enver <enverdavid01@gmail.com>
2021-09-25 17:31:35 -0500

    Listo el todo del profe Daniel

--- src/App.js
+++ src/App.js
@@ -1,44 +1,66 @@
 import React from "react";
 import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TodoItem } from "./components/TodoItem";
 
-const list = [
-  {
-    title: "Alimentar a Mandarina",
-    status: false
-  },
-  {
-    title: "Estudiar JS",
-    status: true
-  },
-  {
-    title: "Preparar comida",
-    status: false
-  }
-]
+// const list = [
+//   {
+//     title: "Alimentar a Mandarina",
+//     status: false
+//   },
+//   {
+//     title: "Estudiar JS",
+//     status: true
+//   },
+//   {
+//     title: "Preparar comida",
+//     status: false
+//   }
+// ]
 
 function App() {
 
-  const [todoTitle, setTodoTitle] = useState(null);
-  const [todoArray, setTodoArray] = useState(list);
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoArray, setTodoArray] = useState([]);
 
   const handleAddTodo = () => {
-    console.log(todoTitle)
-    setTodoArray([...todoArray, {title: todoTitle, status: false}])
+    setTodoArray([...todoArray, {title: todoTitle, status: false}]);
+    setTodoTitle("");
   }
+
+  useEffect(() => {
+    if(JSON.parse(localStorage.getItem("todoList"))) {
+      setTodoArray(JSON.parse(localStorage.getItem("todoList")))
+    }else {
+      setTodoArray(localStorage.setItem( "todoList", JSON.stringify([]) ))
+    }
+  }, []);
+
+  useEffect(()=> {
+    localStorage.setItem("todoList", JSON.stringify(todoArray));
+  }, [todoArray]);
 
   return (
     <div className="h-screen bg-second_green flex justify-center items-center">
       <div className="bg-third_green md:bg-main_green w-full text-white px-10 py-10 rounded md:w-5/12 h-2/3 overflow-scroll">
         <h2 className="text-left text-2xl">TODO list</h2>
-        <AddTodo setTodoTitle={setTodoTitle} handleAddTodo={handleAddTodo}/>
+        <AddTodo 
+          todoTitle={todoTitle} 
+          setTodoTitle={setTodoTitle} 
+          handleAddTodo={handleAddTodo}
+          />
         
           <TodoList>
             {/* children */}
-            {todoArray.map((task, index) => (
-                <TodoItem title={task.title} status={task.status} key={index} />
+            {todoArray.length > 0 && todoArray.map((task, index) => (
+                <TodoItem 
+                  title={task.title} 
+                  status={task.status}
+                  setTodoArray={setTodoArray}
+                  todoArray={todoArray}
+                  index={index}
+                  key={index} />
               ))}
             {/* children */}
           </TodoList>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-const TodoItem = ({title, status}) => {
+const TodoItem = ({title, status, setTodoArray, index, todoArray}) => {
+
+    const completeOrDeleteTodo = () => {
+        if (status) {
+            const myArr = [...todoArray];
+            setTodoArray(myArr.filter((e) => e !== myArr[index]));
+            // myArr.splice(index, 1);
+            // setTodoArray(myArr);
+
+        } else {
+            const myArr = [...todoArray];
+            myArr[index].status = true;
+            setTodoArray(myArr)
+            console.log(todoArray[index].status);        
+        }
+        
+    }
+
     return (
         <div className="flex justify-between item-center w-full mt-5 border-b-2 border-second_green py-3">
-            <p>{title}</p>
-            <button className={`${status ? "bg-third_green" : "bg-transparent"} px-5 py-2 text-iron_blue`}>
+            <p className={`${status ? "line-through" : ""}`}>{title}</p>
+            <button 
+                className={`${status ? "bg-third_green" : "bg-transparent"} px-5 py-2 text-iron_blue`}
+                onClick={completeOrDeleteTodo}
+                >
                 {status ? "Borrar" : "Completar"}
             </button>
         </div>
